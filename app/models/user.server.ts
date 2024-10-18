b04777ef70e5c822670f5007e5ffc64ce0cce4d6
Joonsu Yoon <bananamilk452@gmail.com>
2024-10-18 22:22:24 +0900

    유저 모델, 세션 모듈 추가

---
+++ app/models/user.server.ts
@@ -0,0 +1,42 @@
+import type { Password, User } from "@prisma/client";
+import argon2 from "@node-rs/argon2";
+import { prisma } from "~/.server/db";
+
+export type { User };
+
+export async function getUserById(id: User["id"]) {
+  return prisma.user.findUnique({
+    where: { id },
+  });
+}
+
+export async function getUserByEmail(email: User["email"]) {
+  return prisma.user.findUnique({
+    where: { email },
+  });
+}
+
+export async function verifyLogin(
+  email: User["email"],
+  password: Password["hash"],
+) {
+  const userWithPassword = await prisma.user.findUnique({
+    where: { email },
+    include: { password: true },
+  });
+
+  if (!userWithPassword || !userWithPassword.password) {
+    return null;
+  }
+
+  const isValid = await argon2.verify(password, userWithPassword.password.hash);
+
+  if (!isValid) {
+    return null;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { password: _password, ...userWithoutPassword } = userWithPassword;
+
+  return userWithoutPassword;
+}
