3083e74426a7084df5875760dc286d3de647564a
Joonsu Yoon <bananamilk452@gmail.com>
2024-10-19 19:50:54 +0900

    root에서 유저 볼러오기, 메타 태그 추가

--- app/root.tsx
+++ app/root.tsx
@@ -5,10 +5,23 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import type {
+  LinksFunction,
+  LoaderFunctionArgs,
+  MetaFunction,
+} from "@remix-run/node";
 import { createHead } from "remix-island";
 
+import { getUser } from "./session.server";
 import "./tailwind.css";
+
+export const meta: MetaFunction = () => {
+  return [
+    { charSet: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+  ];
+};
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://cdn.jsdelivr.net" },
@@ -35,6 +48,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
     </>
   );
 }
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  return json({ user: await getUser(request) });
+};
 
 export default function App() {
   return <Outlet />;
