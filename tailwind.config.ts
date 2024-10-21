9ecc23aa940cae0ca829421445c0c604e7d64202
Joonsu Yoon <bananamilk452@gmail.com>
2024-10-21 18:30:12 +0900

    shadcn 사이드바 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,12 @@ export default {
     extend: {
       keyframes: {
         onoff: {
-          "0%, 49%": { opacity: "1" },
-          "50%, 100%": { opacity: "0" },
+          "0%, 49%": {
+            opacity: "1",
+          },
+          "50%, 100%": {
+            opacity: "0",
+          },
         },
       },
       animation: {
@@ -72,6 +76,16 @@ export default {
           "3": "hsl(var(--chart-3))",
           "4": "hsl(var(--chart-4))",
           "5": "hsl(var(--chart-5))",
+        },
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
         },
       },
     },
